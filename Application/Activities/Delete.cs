3c523d51df9cef5b1643490e4ef5c371104e17c3
Evghenii <evghenii.sirbu@yahoo.com>
2023-12-24 12:13:30 +0100

    added basic crud operations

---
+++ Application/Activities/Delete.cs
@@ -0,0 +1,39 @@
+using AutoMapper;
+using MediatR;
+using Persistance;
+
+namespace Application.Activities
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Id);
+
+                _context.Remove(activity);
+
+                await _context.SaveChangesAsync();
+
+                return Unit.Value;
+            }
+        }
+    }
+}
