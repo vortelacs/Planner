e574698b0ac43d817e539197b3eca5ab9d3fc057
Evghenii <evghenii.sirbu@yahoo.com>
2023-12-24 16:44:57 +0100

    moved configurations from startup to extension class

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,45 @@
+using Application.Activities;
+using Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Persistance;
+
+namespace API.Extensions
+{
+
+    public static class ApplicationServiceExtensions
+    {
+
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+
+            services.AddSwaggerGen(c =>
+               {
+                   c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
+               });
+
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithOrigins("http://localhost:3000");
+                });
+            });
+
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+
+    }
+}
