1afda0460b484d3941e46430745ed3bf6b973ab4
Evghenii <evghenii.sirbu@yahoo.com>
2023-12-25 20:44:14 +0100

    added activity form component

---
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -0,0 +1,24 @@
+import { Button, Form, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+
+interface Props {
+  activity: Activity | undefined;
+  closeForm: () => void;
+}
+
+export default function ActivityForm({ closeForm, activity }: Props) {
+  return (
+    <Segment clearing>
+      <Form>
+        <Form.Input placeholder="Title" />
+        <Form.TextArea placeholder="Description" />
+        <Form.Input placeholder="Category" />
+        <Form.Input placeholder="Date" />
+        <Form.Input placeholder="City" />
+        <Form.Input placeholder="Venue" />
+        <Button floated="right" positive type="submit" content="Submit" />
+        <Button onClick={closeForm} floated="right" content="Cancel" />
+      </Form>
+    </Segment>
+  );
+}
